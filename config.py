6fa8a07857a594c3a0da27cc317aedec5c9db23c
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2023-12-09 19:23:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+API_ID = int(environ.get("API_ID", "6968528769"))
+API_HASH = environ.get("API_HASH", "57b85f72104db3f08f9795b0410eb556")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6731088496:AAEtC6jM4EVeQH6JPNCBEmhe1TWo1KuBONM")
+ADMIN = int(environ.get("ADMIN", "6968528769"))          
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
